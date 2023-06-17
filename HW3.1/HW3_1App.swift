b35bf4af50fcb1b5523d283858c5f19070ef424e
lax3331 <la2life1234@gmail.com>
2023-06-17 18:56:20 +0300

    Initial Commit

---
+++ HW3.1/HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_1App.swift
+//  HW3.1
+//
+//  Created by Nikita Kuznetsov on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
